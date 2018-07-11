1e1d5027f03e8c4e4624ad64e4da7a2c70f528ff
KarthikArwin <32849822+KarthikArwin@users.noreply.github.com>
2018-07-11 10:31:35 +0530

    Half home page

---
+++ index.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+<title>UnikerZ</title>
+<link rel="stylesheet" href="css/unikerz.css">
+</head>
+
+<body>
+	<header>
+        <div class="header1">
+            <div class="u_wrapper">
+                <div class="u_header_left">
+                  <p>Turning big ideas into great products</p>
+                  <p><i class="fas fa-envelope"></i> unikerz@gmail.com</p>
+                    <p><i class="fas fa-phone"></i> +91 7200 214 742</p>
+                </div>
+                <div class="u_header_right">
+                    <ul class="soc_icons">
+                        <li class="icon">
+                            <i class="fab fa-facebook-f"></i>
+                        </li>
+                        <li class="icon">
+                            <i class="fab fa-twitter"></i>
+                        </li>
+                        <li class="icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </li>
+                        <li class="icon">
+                            <i class="fab fa-google-plus-g"></i>
+                        </li>
+                        <li class="icon">
+                            <i class="fab fa-instagram"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="header2">
+            <div class="u_wrapper">
+            	<div class="logo">
+                	<img src="img/logo.png" alt="UnikerZ">
+                </div>
+            </div>
+        </div>
+        <div class="header3">
+        	<div class="u_wrapper">
+            	<div class="u_main_menu">
+                	<ul class="main-menu">
+                    	<li class="menu-item">
+                        	<a href="index.php">HOME</a>	
+                        </li>
+                        <li class="menu-item">
+                        	<a href="index.php">about us</a>	
+                        </li>
+                        <li class="menu-item">
+                        	<a href="index.php">training</a>	
+                        </li>
+                        <li class="menu-item">
+                        	<a href="index.php">other services</a>	
+                        </li>
+                        <li class="menu-item">
+                        	<a href="index.php">events & gallery</a>	
+                        </li>
+                        <li class="menu-item">
+                        	<a href="index.php">training videos</a>	
+                        </li>
+                        <li class="menu-item">
+                        	<a href="index.php">contact us</a>	
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section id="u_slider">
+    	<div class="slider">
+        	
+        </div>
+    </section>
+    <section id="u_intro">
+    	<div class="u_wrapper">
+    	<div class="intro1">
+        	<h2 class="heading">Get full range of<br>Premium Industrial<br>Services for your business</h2>
+            <div class="intro1-1">
+            	<div class="intro-1-img">
+                	<img src="img/home2.png" alt="intro">
+                </div>
+                <div class="intro-1-para">
+                	<p>The ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis em aperiam, eaque ipsa quae ab illo inventore </p>
+                </div>
+            </div>
+            <div class="intro-2">
+            	<p>The ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore
+mque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis,we he ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa e ab illo inventore veritatisqua</p>
+            </div>
+        </div>
+        <div class="intro2">
+        	<h2>FEATURE EVENTS</h2>
+            <div class="event">
+            	<div class="event-img">
+                	<img src="img/home1.png" alt="">
+                </div>
+                <div class="event-des">
+                	<h4>LOREm ipsum dollar is emitta</h4>
+                    <div class="author">
+                    	<i class="fas fa-user-circle"></i> Mathan
+                    </div>
+                    <div class="date">
+                    	<i class="far fa-calendar-alt"></i> June 26, 2018
+                    </div>
+                    <div class="event-para">
+                    	<p>Amnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis</p>
+                    </div>
+                    <div class="event-btn">
+                    	<button>REGISTER HERE</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <section id="u_parallax">
+    	<div class="parallax">
+        	<div class="parallax-in">
+            	<p>The great aim of education is not knowledge, but action</p>
+            </div>
+        </div>
+    </section>
+    <section id="u_service">
+    	<div class="u_wrapper">
+    	<div class="services">
+        	<div class="head">
+            	<h2>Our Service</h2>
+                <p>The ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis, r sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae remsit voluptatem </p>
+            </div>
+            <div class="ser-sec">
+            	<div class="ser ser1">
+                	<div class="ser-in">
+                    	<h3>Training</h3>
+                        <img src="img/icon1.png" alt="">
+                        <p>It’s a place engineers can rely on for Quality Training. We are committed to driving the right ...</p>
+                        <button>Read More</button>
+                    </div>
+                </div>
+                <div class="ser ser1">
+                	<div class="ser-in">
+                    	<h3>Other services</h3>
+                        <img src="img/icon2.png" alt="">
+                        <p>Presentation of theory is easy, but the development of a top performing professional is not. </p>
+                        <button>Read More</button>
+                    </div>
+                </div>
+                <div class="ser ser1">
+                	<div class="ser-in">
+                    	<h3>Seminars</h3>
+                        <img src="img/icon3.png" alt="">
+                        <p>Our participants come from wide range of engineering including Electrical, Electronics, </p>
+                        <button>Read More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <section id="u_videos">
+    	<div class="u_wrapper">
+    	<div class="head">
+            <h2>Latest videos</h2>
+            <p>The ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis</p>
+        </div>
+        </div>
+        <div class="videos">
+        	<div class="video">
+            	<img src="img/vid1.png" alt="">
+            </div>
+            <div class="video">
+            	<img src="img/vid2.png" alt="">
+            </div>
+            <div class="video">
+            	<img src="img/vid3.png" alt="">
+            </div>
+            <div class="video">
+            	<img src="img/vid1.png" alt="">
+            </div>
+        </div>
+    </section>
+    <footer>
+    	<div class="footer">
+        	<div class="foot foot1">
+            	<div class="foot-in">
+                	some
+                </div>
+            </div>
+            <div class="foot foot1">
+            	<div class="foot-in">
+                	some
+                </div>
+            </div>
+            <div class="foot foot1">
+            	<div class="foot-in">
+                	some
+                </div>
+            </div>
+            <div class="foot foot1">
+            	<div class="foot-in">
+                	some
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
