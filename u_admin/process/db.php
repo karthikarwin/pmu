f6698907c56d139783894ac10b39f7749933d660
KarthikArwin <32849822+KarthikArwin@users.noreply.github.com>
2018-08-01 10:33:12 +0530

    Admin

--- u_admin/process/db.php
+++ u_admin/process/db.php
@@ -1,12 +1,12 @@
 <?php
 
-/* $u_host = "unikerz.aakkamcreations.com";
+$u_host = "unikerz.aakkamcreations.com";
 $u_user = "unikerz_db";
-$u_pwd = "Uni1kerz@1"; */
+$u_pwd = "Uni1kerz@1";
 
-$u_host = "localhost";
+/* $u_host = "localhost";
 $u_user = "root";
-$u_pwd = "";
+$u_pwd = ""; */
 
 $db_name = "unikerz";
 
